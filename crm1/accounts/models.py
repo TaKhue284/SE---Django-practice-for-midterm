54119722a60c3df67b5ff63470c6b271a705cf76
TaKhue284 <khueta2804@gmail.com>
2022-04-19 11:29:19 +0700

    Create,Update,Delete

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -13,10 +13,12 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+        
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
     def __str__(self):
         return self.name
+
 class Product(models.Model):
     CATEGORY = (
 			('Indoor', 'Indoor'),
@@ -31,6 +33,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 class Order(models.Model):
     STATUS = (
 			('Pending', 'Pending'),
@@ -41,3 +44,6 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
+    def __str__(self):
+        return self.product.name
